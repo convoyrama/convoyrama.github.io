863f2a756d968ec354e57aef2f8e9cb66d19ee65
will <wilsonrfros@gmail.com>
2025-10-18 20:21:59 -0300

    Revert "update"
    This reverts commit cb68c77dcb53d9d5b3b5c825b448b23a66210949.

--- license_generator/js/fix_canvasv2.sh
+++ license_generator/js/fix_canvasv2.sh
@@ -8,7 +8,4 @@ cp "$FILE" "${FILE}.bak"
 # Remove trailing backslashes from lines in the file
 sed -i 's/\\$//' "$FILE"
 
-# Replace \' with '
- sed -i "s/\\\'/\'/g" "$FILE"
-
-echo "Trailing backslashes and \\' sequences removed from $FILE. A backup was created at ${FILE}.bak"
+echo "Trailing backslashes removed from $FILE. A backup was created at ${FILE}.bak"
