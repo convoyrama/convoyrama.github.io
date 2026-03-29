d59812dd34fb3e0a6d267cb661d187f54b5c5396
convoyrama <167719926+convoyrama@users.noreply.github.com>
2026-03-29 00:46:03 -0300

    update web player

---
+++ lagfm/sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'lagfm-v1';
+const ASSETS = [
+  'lagfm_web.html',
+  'streams.json',
+  'icon.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
