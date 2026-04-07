2a7276565f41762c2c1a404ad45afe3c7c3137b4
convoyrama <167719926+convoyrama@users.noreply.github.com>
2026-04-07 01:08:29 -0300

    update

--- lagfm/sw.js
+++ lagfm/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lagfm-v1';
+const CACHE_NAME = 'lagfm-v2'; // Incrementamos versión para forzar actualización
 const ASSETS = [
   'lagfm_web.html',
   'streams.json',
@@ -6,8 +6,19 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting(); // Forzamos a que el nuevo SW tome el control de inmediato
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
   );
 });
 
